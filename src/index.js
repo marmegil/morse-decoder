b3b513cacc7a5db5c88c0d132e96cdf4751daa57
marmegil <kirill.german50@gmail.com>
2020-10-23 21:49:53 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -35,10 +35,34 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let tmpArr = [];
+    let resultArr = [];
+    let morseArr = [];
+    let symbolSize = 5;
+
+    for (let i = 0; i < expr.length; i += 2) {
+        if (expr[i] === '0') morseArr.push('');
+        else if (expr[i + 1] === '0') morseArr.push('.');
+        else if (expr[i + 1] === '1') morseArr.push('-');
+        else morseArr.push(' ');
+    } 
+
+    for (i = 0; i < morseArr.length / symbolSize; i++) {
+      tmpArr[i] = morseArr.slice(i * symbolSize, i * symbolSize + symbolSize);     
+    }
+
+    for (i = 0; i < tmpArr.length; i++) {
+        for (let prop in MORSE_TABLE) {
+            if (tmpArr[i].join('') === prop) resultArr.push(MORSE_TABLE[prop]);
+        }
+    }
+
+    return resultArr.join('');
 }
 
 module.exports = {
